acc20756515ca82a2699753d8be53abcf7b9f1d4
Charles Gao <1030587461@qq.com>
2023-04-25 19:27:38 +0800

    finished inference code with transformers kit

--- infer_config.py
+++ infer_config.py
@@ -1,3 +1,9 @@
+import torch.cuda as cuda
+
+device = 'cuda:0' if cuda.is_available() else 'cpu'
+max_length = 512
 temperature = 0.5
 top_p = 0.7
-max_length = 128
+repetition_penalty = 1.02
+
+
--- transformer_train/infer.py
+++ transformer_train/infer.py
@@ -1,40 +1,46 @@
 from transformers import AutoModel
 from transformers import AutoTokenizer
 import torch
-from peft import PeftModel
+from peft import LoraConfig, TaskType, get_peft_model
 import infer_config
 
-net = AutoModel.from_pretrained('THUDM/chatglm-6b', trust_remote_code=True, load_in_8bit=True, device_map='auto')
+torch.set_default_tensor_type(torch.cuda.HalfTensor)
+net = AutoModel.from_pretrained('THUDM/chatglm-6b', trust_remote_code=True, device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained('THUDM/chatglm-6b', trust_remote_code=True)
-net = PeftModel.from_pretrained(net, './saved_models/epoch_3')
+
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    inference_mode=True,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+)
+net = get_peft_model(net, peft_config)
+net.load_state_dict(torch.load('./saved_models/epoch_3/adapter_model.bin'), strict=False)
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 
 def inference(text):
     input_ids = tokenizer.encode(text)
     input_ids = torch.LongTensor([input_ids])
+    input_ids = input_ids.to(infer_config.device)
     out = net.generate(
         input_ids=input_ids,
         max_length=infer_config.max_length,
         temperature=infer_config.temperature,
         top_p=infer_config.top_p,
-        repetition_penalty=1.02
+        repetition_penalty=infer_config.repetition_penalty
     )
-    answer = tokenizer.decode(out[0])\
-                      .replace(text, '')\
-                      .replace('\nEND', '').strip()
+    answer = tokenizer.decode(out[0])
     return answer
 
 
 def chat():
-    history = []
     with torch.no_grad():
         while True:
             text = input('User: ')
-            text = 'Instruction: ' + text + 'Answer: '
-            history.append(text)
-            text_with_history = '\n'.join(history[-3:])
-            answer = inference(text_with_history)
-            history.append(f'Answer: {answer}')
+            answer = inference(text)
+            print(f'XiaoRuan: {answer}')
 
 
 if __name__ == '__main__':
